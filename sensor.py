31d120cd97bff9efcd95afaee8fdb18c83dde80e
Ryan Claussen <ryan.claussen@gmail.com>
2019-06-13 22:18:21 -0500

    Fix None access
    Check if the device tracker exists before trying to access its properties.

--- sensor.py
+++ sensor.py
@@ -330,7 +330,8 @@ class Places(Entity):
 
         home_latitude = str(hass.states.get(home_zone).attributes.get('latitude'))
         home_longitude = str(hass.states.get(home_zone).attributes.get('longitude'))
-        self._entity_picture = hass.states.get(devicetracker_id).attributes.get('entity_picture')
+        if hass.states.get(devicetracker_id) in not None:
+            self._entity_picture = hass.states.get(devicetracker_id).attributes.get('entity_picture')
         self._street_number = None
         self._street = None
         self._city = None
